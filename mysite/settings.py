659c88a2ef70830485bdd4311f641ff0718dd205
dishu001 <2020eeb1167@iitrpr.ac.in>
2022-06-26 16:12:30 +0530

    final commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 MEDIA_ROOT = '/media/'
 
@@ -140,3 +142,4 @@ LOGINPROF_REDIRECT_URL = '/'
 MESSAGE_TAGS={
     messages.ERROR : 'danger',
 }
+
